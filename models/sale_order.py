59c8bfc2c135bc98dfcf821fa3e515d533848bd0
Andrés Fernando Piedra <panaonchoatu@hotmail.com>
2026-09-17 08:43:02 -0500

    Agregación de Método webverso_portal_link
    Método que genera el link del método de pago en Odoo

--- models/sale_order.py
+++ models/sale_order.py
@@ -15,9 +15,8 @@ class SaleOrder(models.Model):
             pdf, _ = report._render_qweb_pdf(report_ref=report.report_name, res_ids=self.ids)
 
             return {
-                "success": True,
                 "filename": f"{self.name}.pdf",
-                "content": base64.b64encode(pdf).decode(),
+                "contentBase64": base64.b64encode(pdf).decode(),
                 "mimeType": "application/pdf"
             }
 
@@ -31,4 +30,30 @@ class SaleOrder(models.Model):
 
             raise UserError(
                 f"Error generando el PDF de la cotización SIIII {self.name}: {message}"
+            )
+
+    def webverso_portal_link(self):
+        self.ensure_one()
+
+        try:
+            self._portal_ensure_token()
+
+            relative_url = self.get_portal_url()
+            base_url = self.env["ir.config_parameter"].sudo().get_param("web.base.url")
+
+            if relative_url.startswith("http"):
+                portal_url = relative_url
+            else:
+                portal_url = f"{base_url}{relative_url}"
+
+            return {
+                "id": self.id,
+                "name": self.name,
+                "state": self.state,
+                "portalUrl": portal_url
+            }
+
+        except Exception as e:
+            raise UserError(
+                f"Error generando el enlace del portal para {self.name}: {str(e)}"
             )
